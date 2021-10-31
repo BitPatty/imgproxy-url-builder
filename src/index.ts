22b64cd2659b89384ad69d4d5de0459899662591
Matteias Collet <matteias.collet@bluewin.ch>
2021-10-31 23:16:35 +0000

    export all option types and enums

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,78 @@ import pb, { ParamBuilder } from './param-builder';
 
 import GravityType from './enums/gravity-type.enum';
 import ResizeType from './enums/resize-type.enum';
+import ResizingAlgorithm from './enums/resizing-algorithm.enum';
+import UnsharpeningMode from './enums/unsharpening-mode.enum';
 import WatermarkPosition from './enums/watermark-position.enum';
 
+import { BackgroundOptions } from './transformers/background';
+import { BackgroundAlphaOptions } from './transformers/background-alpha';
+import { BlurOptions } from './transformers/blur';
+import { CacheBusterOptions } from './transformers/cache-buster';
+import { ContrastOptions } from './transformers/contrast';
+import { CropOptions } from './transformers/crop';
+import { DprOptions } from './transformers/dpr';
+import { FileNameOptions } from './transformers/filename';
+import { FormatOptions } from './transformers/format';
+import { GifOptions } from './transformers/gif-options';
+import { GravityOptions } from './transformers/gravity';
+import { JpegOptions } from './transformers/jpeg-options';
+import { MaxBytesOptions } from './transformers/max-bytes';
+import { PaddingOptions } from './transformers/pad';
+import { PageOptions } from './transformers/page';
+import { PixelateOptions } from './transformers/pixelate';
+import { PngOptions } from './transformers/png-options';
+import { PresetOptions } from './transformers/preset';
+import { QualityOptions } from './transformers/quality';
+import { ResizeOptions } from './transformers/resize';
+import { ResizingAlgorithmOptions } from './transformers/resizing-algorithm';
+import { RotationOptions } from './transformers/rotate';
+import { SaturationOptions } from './transformers/saturation';
+import { SharpenOptions } from './transformers/sharpen';
+import { TrimOptions } from './transformers/trim';
+import { UnsharpeningOptions } from './transformers/unsharpen';
+import { VideoThumbnailSecondOptions } from './transformers/video-thumbnail-second';
+import { WatermarkOptions } from './transformers/watermark';
+import { WatermarkUrlOptions } from './transformers/watermark-url';
+
 export default pb;
-export { ParamBuilder, GravityType, ResizeType, WatermarkPosition };
+export {
+  // Param builder itself
+  ParamBuilder,
+  // Enums
+  GravityType,
+  ResizeType,
+  ResizingAlgorithm,
+  UnsharpeningMode,
+  WatermarkPosition,
+  // Modifiers
+  BackgroundOptions,
+  BackgroundAlphaOptions,
+  BlurOptions,
+  CacheBusterOptions,
+  ContrastOptions,
+  CropOptions,
+  DprOptions,
+  FileNameOptions,
+  FormatOptions,
+  GifOptions,
+  GravityOptions,
+  JpegOptions,
+  MaxBytesOptions,
+  PaddingOptions,
+  PageOptions,
+  PixelateOptions,
+  PngOptions,
+  PresetOptions,
+  QualityOptions,
+  ResizeOptions,
+  ResizingAlgorithmOptions,
+  RotationOptions,
+  SaturationOptions,
+  SharpenOptions,
+  TrimOptions,
+  UnsharpeningOptions,
+  VideoThumbnailSecondOptions,
+  WatermarkOptions,
+  WatermarkUrlOptions,
+};
