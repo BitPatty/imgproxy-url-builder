4a5874ed28dafeabfb5e25beacedc2056254b2ea
Matteias Collet <matteias.collet@bluewin.ch>
2021-10-31 14:27:22 +0000

    add jest globals

--- test/transformers/rotate.test.ts
+++ test/transformers/rotate.test.ts
@@ -4,24 +4,13 @@ import getPixels from 'get-pixels';
 import { promisify } from 'util';
 import TestImage from '../test-utils/test-image';
 import { RotationOptions } from '../../src/transformers/rotate';
-
-const key =
-  '91bdcda48ce22cd7d8d3a0eda930b3db1762bc1cba5dc13542e723b68fe55d6f9d18199cbe35191a45faf22593405cad0fe76ffec67d24f8aee861ac8fe44d96';
-const salt =
-  '72456c286761260f320391fe500fcec53755958dabd288867a6db072e1bc1dbd84b15079838a83a715edc1ecad50c3ce91dd8fdef6f981816fa274f91d8ecf06';
+import jestGlobals from '../../jest.globals';
 
 describe('Rotate', () => {
   test.each([90, 180, 270])('Valid Rotation: %i', async (angle) => {
     const res = pb()
       .rotate(angle as RotationOptions)
-      .build({
-        path: 's3://test-bucket/test-image.png',
-        baseUrl: 'http://localhost:4000',
-        signature: {
-          key,
-          salt,
-        },
-      });
+      .build(jestGlobals.buildOptions);
 
     await expect(axios.head(res)).resolves.toHaveProperty('status', 200);
 
@@ -72,14 +61,7 @@ describe('Rotate', () => {
   test.each([1, 123, 345])('Invalid Rotation: %i', async (angle) => {
     const res = pb()
       .rotate(angle as RotationOptions)
-      .build({
-        path: 's3://test-bucket/test-image.png',
-        baseUrl: 'http://localhost:4000',
-        signature: {
-          key,
-          salt,
-        },
-      });
+      .build(jestGlobals.buildOptions);
 
     await expect(axios.get(res)).rejects.toHaveProperty(
       'response.data',
