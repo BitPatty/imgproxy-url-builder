5ab5bfcbb839b78e5e8c5e1b8685cfeafef15ca5
Matteias Collet <matteias.collet@bluewin.ch>
2021-10-31 04:20:31 +0000

    add some sample tests

---
+++ test/plain.test.ts
@@ -0,0 +1,37 @@
+import pb from '../src';
+import axios from 'axios';
+
+const key =
+  '91bdcda48ce22cd7d8d3a0eda930b3db1762bc1cba5dc13542e723b68fe55d6f9d18199cbe35191a45faf22593405cad0fe76ffec67d24f8aee861ac8fe44d96';
+const salt =
+  '72456c286761260f320391fe500fcec53755958dabd288867a6db072e1bc1dbd84b15079838a83a715edc1ecad50c3ce91dd8fdef6f981816fa274f91d8ecf06';
+
+describe('Plain / Encoded', () => {
+  test('Plain', async () => {
+    const res = pb().build({
+      path: 's3://test-bucket/test-image.png',
+      baseUrl: 'http://localhost:4000',
+      plain: true,
+      signature: {
+        key,
+        salt,
+      },
+    });
+
+    await expect(axios.get(res)).resolves.toHaveProperty('status', 200);
+  });
+
+  test('Encoded', async () => {
+    const res = pb().build({
+      path: 's3://test-bucket/test-image.png',
+      baseUrl: 'http://localhost:4000',
+      plain: false,
+      signature: {
+        key,
+        salt,
+      },
+    });
+
+    await expect(axios.get(res)).resolves.toHaveProperty('status', 200);
+  });
+});
